297d53533ca65e913f7e0d922a46e80b195b16c2
Grikh-UU-Py <5186415@mail.ru>
2024-09-04 18:30:21 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+# Задача "Нули ничто, отрицание недопустимо!":
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(my_list)
+index = 0
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+        print(my_list[index])
+    if my_list[index] > 0:
+        print(my_list[index])
+    index += 1
